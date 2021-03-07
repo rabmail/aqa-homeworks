927e891f5511bc422c2e4856c44833bc6c3d16b9
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-03-07 19:55:37 +0300

    Edit TestNG

--- src/test/java/ru/netology/domain/BonusServiceTest.java
+++ src/test/java/ru/netology/domain/BonusServiceTest.java
@@ -32,31 +32,5 @@ public class BonusServiceTest {
 
         assertEquals( actual, expected);
     }
-    @org.junit.Test
-    public void shouldReturn10IfAmountIs2000_2() {
 
-        //подготовка данных
-        BonusService service = new BonusService();
-        int amount = 2000;
-
-        //выполняемое действие
-        int actual = service.remain(amount);
-
-        //ожидаемый результат
-        int expected = 0;
-
-        assertEquals(expected, actual);
-    }
-
-    @org.junit.Test
-    public void shouldReturnZeroIfAmountLowerThan1000_2() {
-        BonusService service = new BonusService();
-        int amount = 900;
-
-        int actual = service.remain(amount);
-
-        int expected = 100;
-
-        assertEquals( expected, actual);
-    }
 }
