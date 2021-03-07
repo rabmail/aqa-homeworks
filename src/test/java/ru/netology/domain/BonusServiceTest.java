82c75131d63165bddc837efc5ea62947ee7bc7f8
Dmitriy Dolmatov <Rabmail@mail.ru>
2021-03-07 20:08:38 +0300

    Edit J4_platform

--- src/test/java/ru/netology/domain/BonusServiceTest.java
+++ src/test/java/ru/netology/domain/BonusServiceTest.java
@@ -1,6 +1,8 @@
 package ru.netology.domain;
 
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.Assert.assertEquals;
 
 public class BonusServiceTest {
@@ -23,6 +25,35 @@ public class BonusServiceTest {
 
     @org.junit.Test
     public void shouldReturnZeroIfAmountLowerThan1000() {
+        BonusService service = new BonusService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+
+        int expected = 100;
+
+        assertEquals( expected, actual);
+    }
+
+
+    @Test
+    public void shouldReturn10IfAmountIs2000_1() {
+
+        //подготовка данных
+        BonusService service = new BonusService();
+        int amount = 2000;
+
+        //выполняемое действие
+        int actual = service.remain(amount);
+
+        //ожидаемый результат
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnZeroIfAmountLowerThan1000_2() {
         BonusService service = new BonusService();
         int amount = 900;
 
