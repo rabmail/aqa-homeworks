8309607c163fd9ec55b1a555c2aa9121cb0e199c
Rabmail <Rabmail@mail.ru>
2021-03-09 13:51:37 +0300

    Edit branch junit4-platform

--- src/test/java/ru/netology/domain/TestCashbackHackService.java
+++ src/test/java/ru/netology/domain/TestCashbackHackService.java
@@ -5,19 +5,16 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class BonusServiceTest {
+public class TestCashbackHackService {
 
     @org.junit.Test
     public void shouldReturn10IfAmountIs2000() {
 
-        //подготовка данных
-        BonusService service = new BonusService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 2000;
 
-        //выполняемое действие
         int actual = service.remain(amount);
 
-        //ожидаемый результат
         int expected = 0;
 
         assertEquals(expected, actual);
@@ -25,28 +22,25 @@ public class BonusServiceTest {
 
     @org.junit.Test
     public void shouldReturnZeroIfAmountLowerThan1000() {
-        BonusService service = new BonusService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
 
         int expected = 100;
 
-        assertEquals( expected, actual);
+        assertEquals(expected, actual);
     }
 
 
     @Test
     public void shouldReturn10IfAmountIs2000_1() {
 
-        //подготовка данных
-        BonusService service = new BonusService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 2000;
 
-        //выполняемое действие
         int actual = service.remain(amount);
 
-        //ожидаемый результат
         int expected = 0;
 
         assertEquals(expected, actual);
@@ -54,13 +48,13 @@ public class BonusServiceTest {
 
     @Test
     public void shouldReturnZeroIfAmountLowerThan1000_2() {
-        BonusService service = new BonusService();
+        CashbackHackService service = new CashbackHackService();
         int amount = 900;
 
         int actual = service.remain(amount);
 
         int expected = 100;
 
-        assertEquals( expected, actual);
+        assertEquals(expected, actual);
     }
 }
